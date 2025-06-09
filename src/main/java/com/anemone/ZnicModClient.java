db6db47f76c13d3060601046a47c250a428e83f0
mac <mac@macdeMac-mini.local>
2025-06-09 16:40:02 +0800

    Initial commit

---
+++ src/main/java/com/anemone/ZnicModClient.java
@@ -0,0 +1,10 @@
+package com.anemone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZnicModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/anemone/ZnicModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.anemone;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ZnicModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
